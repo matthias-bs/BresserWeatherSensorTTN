9722e181a0c05347d932eb62105a7d02462c7efb
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2022-09-05 20:21:29 +0200

    Doxygen comments

--- BresserWeatherSensorTTN.ino
+++ BresserWeatherSensorTTN.ino
@@ -252,7 +252,7 @@ protected:
 /*!
  * \class cSensor
  * 
- * \brief The sensor object - collect sensor data ans schedule uplink
+ * \brief The sensor object - collect sensor data and schedule uplink
  */
 class cSensor {
 public:
@@ -363,68 +363,67 @@ const cMyLoRaWAN::lmic_pinmap myPinMap = {
 
 // The following variables are stored in the ESP32's RTC RAM -
 // their value is retained after a Sleep Reset.
-RTC_DATA_ATTR uint32_t                        magicFlag1;
-RTC_DATA_ATTR Arduino_LoRaWAN::SessionState   rtcSavedSessionState;
-RTC_DATA_ATTR uint32_t                        magicFlag2;
-RTC_DATA_ATTR Arduino_LoRaWAN::SessionInfo    rtcSavedSessionInfo;
-RTC_DATA_ATTR size_t                          rtcSavedNExtraInfo;
-RTC_DATA_ATTR uint8_t                         rtcSavedExtraInfo[EXTRA_INFO_MEM_SIZE];
-RTC_DATA_ATTR bool                            runtimeExpired = false;
-RTC_DATA_ATTR time_t                          rtcLastClockSync = 0;
+RTC_DATA_ATTR uint32_t                        magicFlag1;               //!< flag for validating Session State in RTC RAM 
+RTC_DATA_ATTR Arduino_LoRaWAN::SessionState   rtcSavedSessionState;     //!< Session State saved in RTC RAM
+RTC_DATA_ATTR uint32_t                        magicFlag2;               //!< flag for validating Session Info in RTC RAM 
+RTC_DATA_ATTR Arduino_LoRaWAN::SessionInfo    rtcSavedSessionInfo;      //!< Session Info saved in RTC RAM
+RTC_DATA_ATTR size_t                          rtcSavedNExtraInfo;       //!< size of extra Session Info data
+RTC_DATA_ATTR uint8_t                         rtcSavedExtraInfo[EXTRA_INFO_MEM_SIZE]; //!< extra Session Info data
+RTC_DATA_ATTR bool                            runtimeExpired = false;   //!< flag indicating if runtime has expired at least once
+RTC_DATA_ATTR time_t                          rtcLastClockSync = 0;     //!< timestamp of last RTC synchonization to network time
 
 #ifdef ADC_EN
     // ESP32 ADC with calibration
-    ESP32AnalogRead adc;
+    ESP32AnalogRead adc; //!< ADC object for supply voltage measurement
 #endif
 
 // ESP32 ADC with calibration
 #if defined(ADC_EN) && defined(PIN_ADC0_IN)
-    ESP32AnalogRead adc0;
+    ESP32AnalogRead adc0; //!< ADC object
 #endif
 #if defined(ADC_EN) && defined(PIN_ADC1_IN)
-    ESP32AnalogRead adc1;
+    ESP32AnalogRead adc1; //!< ADC object
 #endif
 #if defined(ADC_EN) && defined(PIN_ADC2_IN)
-    ESP32AnalogRead adc2;
+    ESP32AnalogRead adc2; //!< ADC object
 #endif
 #if defined(ADC_EN) && defined(PIN_ADC3_IN)
-    ESP32AnalogRead adc3;
+    ESP32AnalogRead adc3; //!< ADC object
 #endif
 
-// Setup Bresser Weather Sensor
+/// Bresser Weather Sensor Receiver
 WeatherSensor weatherSensor;
 
 
 #ifdef ONEWIRE_EN
     // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-    OneWire oneWire(PIN_ONEWIRE_BUS);
+    OneWire oneWire(PIN_ONEWIRE_BUS); //!< OneWire bus
 
     // Pass our oneWire reference to Dallas Temperature. 
-    DallasTemperature temp_sensors(&oneWire);
+    DallasTemperature temp_sensors(&oneWire); //!< Dallas temperature sensors connected to OneWire bus
 #endif
 
 #ifdef MITHERMOMETER_EN
     // Setup BLE Temperature/Humidity Sensors
-    ATC_MiThermometer miThermometer(knownBLEAddresses);
+    ATC_MiThermometer miThermometer(knownBLEAddresses); //!< Mijia Bluetooth Low Energy Thermo-/Hygrometer
 #endif
 
-// Uplink payload buffer
-static uint8_t loraData[PAYLOAD_SIZE];
+/// LoRaWAN uplink payload buffer
+static uint8_t loraData[PAYLOAD_SIZE]; //!< LoRaWAN uplink payload buffer
 
-// Sleep request (set in NetTxComplete();
+/// Sleep request (set in NetTxComplete();
 bool sleepReq = false;
 
-// Force sleep mode after <sleepTimeout> has been reached (if FORCE_SLEEP is defined) 
-ostime_t sleepTimeout;
+/// Force sleep mode after <code>sleepTimeout</code> has been reached (if FORCE_SLEEP is defined) 
+ostime_t sleepTimeout; //!
 
-// Seconds since the UTC epoch
+/// Seconds since the UTC epoch
 uint32_t userUTCTime;
 
-// RTC sync request - 
-// set (if due) in setup() / cleared in UserRequestNetworkTimeCb()
+/// RTC sync request flag - set (if due) in setup() / cleared in UserRequestNetworkTimeCb()
 bool rtcSyncReq = false;
 
-// Real time clock
+/// Real time clock
 ESP32Time rtc;
 
 /****************************************************************************\
@@ -460,13 +459,13 @@ ESP32Time rtc;
     // The following constants should be copied to secrets.h and configured appropriately
     // according to the settings from TTN Console
     
-    // deveui, little-endian (lsb first)
+    /// DeviceEUI, little-endian (lsb first)
     static const std::uint8_t deveui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     
-    // appeui, little-endian (lsb first)
+    /// AppEUI, little-endian (lsb first)
     static const std::uint8_t appeui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     
-    // appkey: just a string of bytes, sometimes referred to as "big endian".
+    /// AppKey: just a string of bytes, sometimes referred to as "big endian".
     static const std::uint8_t appkey[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 #endif
 
@@ -476,13 +475,12 @@ ESP32Time rtc;
 |
 \****************************************************************************/
 
+/// Arduino setup
 void setup() {
     // set baud rate
     Serial.begin(115200);
 
-    // wait for serial to be ready
-    //while (! Serial)
-    //    yield();
+    // wait for serial to be ready - or timeout if USB is not connected
     delay(500);
 
     sleepTimeout = sec2osticks(SLEEP_TIMEOUT_INITIAL);
@@ -519,6 +517,7 @@ void setup() {
 |
 \****************************************************************************/
 
+/// Arduino execution loop
 void loop() {
     // the order of these is arbitrary, but you must poll them all.
     myLoRaWAN.loop();
@@ -822,7 +821,7 @@ cMyLoRaWAN::GetAbpProvisioningInfo(AbpProvisioningInfo *pAbpInfo) {
     return true;
 }
 
-// Print date and time (i.e. local time)
+/// Print date and time (i.e. local time)
 void printDateTime(void) {
         struct tm timeinfo;
         char tbuf[26];
@@ -833,7 +832,15 @@ void printDateTime(void) {
         DEBUG_PRINTF("%s\n", tbuf);
 }
 
-// Callback function for setting RTC from LoRaWAN network time
+/**
+ * \fn UserRequestNetworkTimeCb
+ * 
+ * \brief Callback function for setting RTC from LoRaWAN network time
+ * 
+ * \param pVoidUserUTCTime user supplied buffer for UTC time
+ * 
+ * \param flagSuccess flag indicating if network time request was succesful
+ */
 void UserRequestNetworkTimeCb(void *pVoidUserUTCTime, int flagSuccess) {
     // Explicit conversion from void* to uint32_t* to avoid compiler errors
     uint32_t *pUserUTCTime = (uint32_t *) pVoidUserUTCTime;
