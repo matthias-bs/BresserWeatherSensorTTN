d26c9194cc37c7322f9d358143e73732107cd227
matthias-bs <83612361+matthias-bs@users.noreply.github.com>
2023-10-07 11:11:11 +0000

    Deploying to gh-pages from @ matthias-bs/BresserWeatherSensorTTN@7efbddeff65206c966e13dc7fc7c517667f4041d 🚀

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['lightningproc_85',['lightningProc',['../_bresser_weather_sensor_t_t_n_8ino.html#a2829a7a314e4e8bb9e316ecdd47dd547',1,'BresserWeatherSensorTTN.ino']]],
-  ['longsleep_86',['longSleep',['../_bresser_weather_sensor_t_t_n_8ino.html#ab8da6c078e11175c8c78c576fbb1da6f',1,'BresserWeatherSensorTTN.ino']]]
+  ['longsleep_86',['longSleep',['../_bresser_weather_sensor_t_t_n_8ino.html#a4ed0c548aa27b9565a080169306229a3',1,'BresserWeatherSensorTTN.ino']]]
 ];
--- search/variables_7.js
+++ search/variables_7.js
@@ -3,11 +3,11 @@ var searchData=
   ['raingauge_91',['rainGauge',['../_bresser_weather_sensor_t_t_n_8ino.html#a6965ff1ec8f32647b494bd1a458575f9',1,'BresserWeatherSensorTTN.ino']]],
   ['rssi_92',['rssi',['../struct_ble_data_s.html#a0eb69624bdc983a6ce9ba9c2e40c2d44',1,'BleDataS']]],
   ['rtc_93',['rtc',['../_bresser_weather_sensor_t_t_n_8ino.html#a9914c8ecdaedd4bd9c93c344bea096c0',1,'BresserWeatherSensorTTN.ino']]],
-  ['rtclastclocksync_94',['rtcLastClockSync',['../_bresser_weather_sensor_t_t_n_8ino.html#a3714d7084c0d8d2cf536f681b65461d6',1,'BresserWeatherSensorTTN.ino']]],
+  ['rtclastclocksync_94',['rtcLastClockSync',['../_bresser_weather_sensor_t_t_n_8ino.html#af8215cbee53251f169443553ad8d7d85',1,'BresserWeatherSensorTTN.ino']]],
   ['rtcsavedextrainfo_95',['rtcSavedExtraInfo',['../_bresser_weather_sensor_t_t_n_8ino.html#a9b7bd5038658fbb5b87d8a9feaedf86f',1,'BresserWeatherSensorTTN.ino']]],
   ['rtcsavednextrainfo_96',['rtcSavedNExtraInfo',['../_bresser_weather_sensor_t_t_n_8ino.html#ad89f826d843e957e7bc15292183af8de',1,'BresserWeatherSensorTTN.ino']]],
   ['rtcsavedsessioninfo_97',['rtcSavedSessionInfo',['../_bresser_weather_sensor_t_t_n_8ino.html#a7962cbb55df5c695bafdf9a3ee1552f4',1,'BresserWeatherSensorTTN.ino']]],
   ['rtcsavedsessionstate_98',['rtcSavedSessionState',['../_bresser_weather_sensor_t_t_n_8ino.html#a6e410fc410377db756bbe398b4636d8d',1,'BresserWeatherSensorTTN.ino']]],
   ['rtcsyncreq_99',['rtcSyncReq',['../_bresser_weather_sensor_t_t_n_8ino.html#a2525adfaa9bcf416f565b423a40a32a6',1,'BresserWeatherSensorTTN.ino']]],
-  ['runtimeexpired_100',['runtimeExpired',['../_bresser_weather_sensor_t_t_n_8ino.html#a1ac3506d3d8af7ec9d55da6acfa1aad4',1,'BresserWeatherSensorTTN.ino']]]
+  ['runtimeexpired_100',['runtimeExpired',['../_bresser_weather_sensor_t_t_n_8ino.html#ac194a1d95d347c904057f714e1a5d43e',1,'BresserWeatherSensorTTN.ino']]]
 ];
